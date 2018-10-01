d2c39bc8c960846d70733b287f35eb361c9207b9
Евгений <1zendalive1@gmail.com>
2018-10-01 20:35:22 +0300

    my first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <title>
+        Страница регистрации
+    </title>
+</head>
+<body>
+<form id="Reg" method="post">
+    <text> Чтобы продожить требуется регистрация</text>
+    <br>
+    <p><label> Ваш логин </label><br>
+        <input id="login" type="text" size="25">
+    </p>
+    <p><label> Ваш пароль </label><br>
+        <input id="password" type="password" size="25">
+    </p>
+    <p><input id="submit" type="submit" value="Зарегистрироваться">
+
+    </p>
+</form>
+</body>
+</html>
